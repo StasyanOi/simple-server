93ac636ff810653702226a3a01e965da191e90b7
StasyanOi <iluvstrwrs@mail.ru>
2021-07-17 17:34:43 +0300

    file loading

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -1,19 +1,35 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class MainServer {
 
-    static ExecutorService executorService = Executors.newFixedThreadPool(3);
+    private static String fileFolder;
+
+    static ExecutorService executorService = Executors.newFixedThreadPool(10);
+    static Logger log = Logger.getLogger(MainServer.class.getName());
 
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8081)) {
+        fileFolder = args[0];
+        log.info("Starting server");
+        int port = 8081;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            log.info("Server starter at port " + port);
             while (true) {
                 Socket accept = serverSocket.accept();
                 executorService.submit(() -> {
@@ -24,67 +40,123 @@ public class MainServer {
                     }
                 });
             }
-        } catch (IOException e) {
+        } catch (IOException | RejectedExecutionException e) {
             e.printStackTrace();
-            executorService.shutdownNow();
         }
+        executorService.shutdown();
     }
+
 
     private static void process(Socket accept) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream()));
 
-        StringBuilder request = new StringBuilder();
-        try (InputStreamReader inputStreamReader = new InputStreamReader(accept.getInputStream())) {
-            while (true) {
-                int read = -10;
-                if (inputStreamReader.ready()) {
-                    read = inputStreamReader.read();
-                } else {
+        requestMatchers(accept, bufferedReader, bufferedWriter);
+    }
+
+    private static void requestMatchers(Socket accept, BufferedReader bufferedReader, BufferedWriter bufferedWriter) throws IOException {
+//        showRequest(accept);
+        List<String> requestLines = getRequest(bufferedReader);
+
+        if (getHomePage(requestLines)) {
+            requestLines.forEach(System.out::println);
+            Path path = Paths.get("Hello.html");
+            List<String> html = Files.lines(path).collect(Collectors.toList());
+            for (int i = 0; i < html.size(); i++) {
+                if (html.get(i).contains("<body>")) {
+                    addFiles(html, i);
                     break;
                 }
-                if (read == -1) {
-                    break;
-                } else if (read == 10) {
-                    request.append((char) 10);
-                } else {
-                    request.append((char) read);
-                }
             }
-
-            HttpRequest httpRequest = getRequest(request);
-            HttpResponse httpResponse = getResponse(200, "Hello world");
-            System.out.println(httpRequest.getRequestLine());
-            System.out.println(httpRequest.getHeaders());
-            System.out.println(httpRequest.getBody());
-
-            try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(accept.getOutputStream())) {
-                outputStreamWriter.write(httpResponse.responseString());
-                outputStreamWriter.flush();
-                inputStreamReader.close();
-                accept.close();
+            bufferedWriter.write(new HttpResponse(200, String.join("\n", html), MimeType.html.getContentType()).responseString());
+            bufferedWriter.flush();
+        } else if (getFile(requestLines)) {
+            String fileName = requestLines.get(0).split(" ")[1];
+            requestLines.forEach(System.out::println);
+            Path path = Paths.get(fileName);
+            if (Files.exists(path)) {
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(accept.getOutputStream());
+                byte[] bytes = Files.readAllBytes(path);
+                byte[] header = HttpResponse.responseHeader(bytes, 200, MimeType.undefined.getContentType()).getBytes(StandardCharsets.UTF_8);
+                byte[] total = concatArrays(header, bytes);
+                bufferedOutputStream.write(total);
+                bufferedOutputStream.flush();
             }
+        } else if (fileForSaving(requestLines)) {
+            requestLines.forEach(System.out::println);
         }
-
+        accept.close();
     }
 
-    private static HttpResponse getResponse(int statusCode, String body) {
-        return new HttpResponse(statusCode, body);
+    private static void showRequest(Socket accept) {
+        try {
+            DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(accept.getInputStream()));
+            System.out.println(dataInputStream.readUTF());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static HttpRequest getRequest(StringBuilder request) {
-        List<String> list = Arrays.asList(request.toString().split("\n"));
-        return new HttpRequest(list);
+    private static boolean fileForSaving(List<String> requestLines) {
+        return requestLines.get(0).contains("POST");
     }
+
+    private static void addFiles(List<String> html, int i) throws IOException {
+        Path path = Paths.get(fileFolder);
+        Files.list(path).forEach(s -> html.add(i, getFileLine(s)));
+    }
+
+    private static String getFileLine(Path s) {
+        String substring = s.toString();
+        String[] split = substring.split("/");
+        return "<a href=\"" + substring + "\">" + split[split.length - 1] + "<a>\n";
+    }
+
+    private static byte[] concatArrays(byte[] header, byte[] bytes) {
+        byte[] total = new byte[header.length + bytes.length];
+        System.arraycopy(header, 0, total, 0, header.length);
+        System.arraycopy(bytes, 0, total, header.length, bytes.length);
+        return total;
+    }
+
+    private static boolean getFile(List<String> requestLines) {
+        if (requestLines.size() != 0) {
+            String[] split = requestLines.get(0).split(" ");
+            Pattern pattern = Pattern.compile("/+.");
+            Matcher matcher = pattern.matcher(split[1]);
+            return split[0].equals("GET") && matcher.find();
+        }
+        return false;
+    }
+
+    private static boolean getHomePage(List<String> requestLines) {
+        if (requestLines.size() != 0) {
+            String[] split = requestLines.get(0).split(" ");
+            return split[0].equals("GET") && split[1].equals("/");
+        }
+        return false;
+    }
+
+    private static List<String> getRequest(BufferedReader bufferedReader) throws IOException {
+        List<String> requestLines = new ArrayList<>();
+        while (bufferedReader.ready()) {
+            String readLine = bufferedReader.readLine();
+            requestLines.add(readLine);
+        }
+        return requestLines;
+    }
+
 
     private static class HttpResponse {
         private final String responseLine;
         private final Map<String, String> headers = new HashMap<>();
         private final String body;
-        private final String crlf = "\r\n";
+        private static final String crlf = "\r\n";
 
-        public HttpResponse(int status, String body) {
+        public HttpResponse(int status, String body, String contentType) {
             responseLine = "HTTP/1.1 " + status;
-            headers.put("Content-Type: ", "text/plain");
-            headers.put("Content-Length: ", "" + body.length());
+            headers.put("Content-Type", contentType);
+            headers.put("Content-Length", "" + body.length());
             this.body = body;
         }
 
@@ -95,53 +167,13 @@ public class MainServer {
             response.append(body);
             return response.toString();
         }
-    }
 
-    private static class HttpRequest {
-        private final String requestLine;
-        private final Map<String, String> headers = new HashMap<>();
-        private final String body;
-
-        public HttpRequest(List<String> request) {
-            requestLine = request.get(0);
-
-            int i = 1;
-            String header;
-            while (!(header = request.get(i++)).equals("\r")) {
-                String[] splitHeader = header.split(":");
-                headers.put(splitHeader[0], splitHeader[1].substring(1, splitHeader[1].length() - 1));
-            }
-
-            StringBuilder body = new StringBuilder();
-            for (int j = i++; j < request.size(); j++) {
-                body.append(request.get(j));
-            }
-            this.body = body.toString();
-        }
-
-        public String getHeader(String headerName) {
-            return headers.get(headerName);
-        }
-
-        public Map<String, String> getHeaders() {
-            return headers;
-        }
-
-        public String getBody() {
-            return body;
-        }
-
-        public String getRequestLine() {
-            return requestLine;
-        }
-
-        @Override
-        public String toString() {
-            return "HttpRequest{" +
-                    "requestLine='" + requestLine + '\n' +
-                    ", headers=" + headers +
-                    ", body='" + body + '\n' +
-                    '}';
+        public static String responseHeader(byte[] body, int status, String contentType) {
+            StringBuilder response = new StringBuilder("HTTP/1.1 " + status + crlf);
+            response.append("Content-Type").append(": ").append(contentType).append(crlf);
+            response.append("Content-Length").append(": ").append(body.length).append(crlf);
+            response.append(crlf);
+            return response.toString();
         }
     }
 }
